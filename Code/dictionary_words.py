716c3a8803bd9cb9061c9eb7e272cbd63092b682
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-01-16 21:57:26 -0500

    added functionality to read in specified number of words from dict/words and create a sentence.

---
+++ Code/dictionary_words.py
@@ -0,0 +1,30 @@
+import random
+import argparse
+
+def create_sentence(words):
+    sentence = ''
+
+    with open('/usr/share/dict/words', 'r') as file:
+        lines = file.readlines()
+    file.close()
+
+    for i in range(words):
+        random_word = random.choice(lines).strip()
+        if i != words - 1:
+            sentence += random_word + ' '
+        else: 
+            sentence += random_word + '.'
+
+    return sentence
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'adds arguments for the sentence creator')
+
+    parser.add_argument('number_of_words', metavar='number_of_words', type=int, help='enter number_of_words you want in the sentence')
+    args = parser.parse_args()
+    number_of_words = args.number_of_words
+
+    sentence = print(create_sentence(number_of_words))
+
+
+    
