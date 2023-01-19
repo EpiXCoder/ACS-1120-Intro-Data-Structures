668413d1c81cc1713e6a87167f5aeba5b22e4a4a
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-01-18 20:24:59 -0500

    refactored to make code DRY

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,30 +1,26 @@
 import random
-import argparse
+import sys
 
 def create_sentence(words):
-    sentence = ''
+    word_list = []
 
     with open('/usr/share/dict/words', 'r') as file:
         lines = file.readlines()
-    file.close()
 
     for i in range(words):
         random_word = random.choice(lines).strip()
-        if i != words - 1:
-            sentence += random_word + ' '
-        else: 
-            sentence += random_word + '.'
+        word_list.append(random_word)
+
+    sentence = f'{" ".join(word_list)}.'
+        # if i != words - 1:
+        #     sentence += random_word + ' '
+        # else: 
+        #     sentence += random_word + '.'
 
     return sentence
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'adds arguments for the sentence creator')
-
-    parser.add_argument('number_of_words', metavar='number_of_words', type=int, help='enter number_of_words you want in the sentence')
-    args = parser.parse_args()
-    number_of_words = args.number_of_words
-
-    print(create_sentence(number_of_words))
+    print(create_sentence(int(sys.argv[1])))
 
 
     
