76361bdc530ebe21a051b6b87ab6e63c18db3ed3
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-01-30 20:22:42 -0500

    render deplay changes

--- Code/app.py
+++ Code/app.py
@@ -1,29 +1,63 @@
-"""Main script, uses other modules to generate sentences."""
-from flask import Flask, request
+from flask import Flask
+app = Flask(__name__)
 import re
 import random
 
 
-from sample import *
+def histogram(source_text):
+    clean_text = re.sub(r"[^\w\d\s-]", "", source_text)
+    word_count = dict()
+    words = clean_text.lower().split()
 
-app = Flask(__name__)
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    # sort by word
+    wordKeys = list(word_count.keys())
+    wordKeys.sort()
+    sorted_by_word = {i: word_count[i] for i in wordKeys}
 
-# TODO: Initialize your histogram, hash table, or markov chain here.
-# Any code placed here will run only once, when the server starts.
+    # sort by count
+    sorted_words_by_count = sorted(word_count.items(), key=lambda x:x[1], reverse=True)
+    sorted_by_count_desc = dict(sorted_words_by_count)
+
+    # Return the needed dictionary
+    # return sorted_by_word
+    return sorted_by_count_desc
+    
+def weighted_selection(histogram):
+    words_and_counts = list(histogram.items())
+    words, weights = zip(*words_and_counts)
+    selected_word = random.choices(words, weights=weights)[0]
+    return selected_word
+    # random_word = random.choices(list(words_histogram.keys()),weights=words_histogram.values(),k=1)[0]
+   
+
+def simulation(histogram, iterations):
+    num_iterations = iterations
+    word_frequencies = {word: 0 for word in histogram.keys()}
+
+    for _ in range(num_iterations):
+        selected_word = weighted_selection(histogram)
+        word_frequencies[selected_word] += 1
+    for word, frequency in word_frequencies.items():
+        if frequency != 0:
+            print(f'{word}: {frequency}')
+
+def sentence_gen(count):
+    # text = 'one- fish two, !fish red "fish" blue fish.'
+    text = open("Kiyosaki.txt","r").read()
+    wordlist = histogram(text)
+    selected_words = []
+    for i in range (count):
+        selected_words.append(weighted_selection(wordlist))
+    sentence = f'{" ".join(selected_words)}.'
+    return sentence.capitalize()
 
 
-@app.route("/")
-def home():
-    """Route that returns a web page containing the generated text."""
-    num_of_words = int(request.args.get("num"))
-    sentence = sentence_gen(num_of_words)
-    return f"<p>{sentence}</p>"
-  
-
-
-
-if __name__ == "__main__":
-    """To run the Flask server, execute `python app.py` in your terminal.
-       To learn more about Flask's DEBUG mode, visit
-       https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
-    app.run(debug=True)
+@app.route('/TweetGen')
+def tweet_deploy():
+    # text = 'one- fish two, !fish red "fish" blue fish.'
+    return(sentence_gen(10))
---
+++ Code/original_app.py
@@ -0,0 +1,29 @@
+"""Main script, uses other modules to generate sentences."""
+from flask import Flask, request
+import re
+import random
+
+
+from sample import *
+
+app = Flask(__name__)
+
+# TODO: Initialize your histogram, hash table, or markov chain here.
+# Any code placed here will run only once, when the server starts.
+
+
+@app.route("/")
+def home():
+    """Route that returns a web page containing the generated text."""
+    num_of_words = int(request.args.get("num"))
+    sentence = sentence_gen(num_of_words)
+    return f"<p>{sentence}</p>"
+  
+
+
+
+if __name__ == "__main__":
+    """To run the Flask server, execute `python app.py` in your terminal.
+       To learn more about Flask's DEBUG mode, visit
+       https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
+    app.run(debug=True)
