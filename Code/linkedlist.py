7a4de965efb9eb1c38c3a10d426aec7b10a866b9
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-02-14 23:17:48 -0500

    LinkedList completed with find test failing. Added replace method because test was asking for it

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -54,6 +54,13 @@ class LinkedList:
         """Return the length of this linked list by traversing its nodes.
         TODO: Running time: O(n) Why and under what conditions?"""
         # TODO: Loop through all nodes and count one for each
+        count = 0 # O(1) time to create new variable
+        node = self.head # O(1) time to assign new variable
+        while node is not None: # Always n iterations because we have to crawl along the whole list
+            node = node.next # O(1) time to reassign variable
+            count += 1 # Not sure about this - but assuming O(1) time to do the addition
+        return count # O(1) time to return variable
+
 
     def append(self, item):
         """Insert the given item at the tail of this linked list.
@@ -61,18 +68,39 @@ class LinkedList:
         # TODO: Create new node to hold given item
         # TODO: If self.is_empty() == True set the head and the tail to the new node
         # TODO: Else append node after tail
+        new_node = Node(item) # O(1) time to assign new variable
+        if self.is_empty(): # O(1) time to check if array is empty
+            self.head = new_node # O(1) time for variable assignment
+            self.tail = new_node # O(1) time for variable assignment
+        else: 
+            self.tail.next = new_node # O(1) time for variable assignment
+            self.tail = new_node # O(1) time for variable assignment
 
     def prepend(self, item):
         """Insert the given item at the head of this linked list.
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Create new node to hold given item
         # TODO: Prepend node before head, if it exists
+        new_node = Node(item) # O(1) time to assign new variable
+        if self.head is not None: # O(1) time to check if node is empty
+            new_node.next = self.head # O(1) time for variable assignment
+            self.head = new_node # O(1) time for variable assignment
+        else: 
+            self.head = new_node # O(1) time for variable assignment
+            self.tail = new_node # O(1) time for variable assignment
 
     def find(self, matcher):
         """Return an item from this linked list if it is present.
         TODO: Best case running time: O(???) Why and under what conditions?
         TODO: Worst case running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all nodes to find item, if present return True otherwise False
+        node = self.head # O(1) time for variable assignment
+        while node is not None: # Always n iterations because we have to crawl along the whole list
+            if node.data == matcher: # O(1) time to check match
+                return True # O(1) time to return boolean
+            else: 
+                node = node.next # O(1) time for variable assignment
+        return False # O(1) time to return boolean
 
     def delete(self, item):
         """Delete the given item from this linked list, or raise ValueError.
@@ -83,6 +111,45 @@ class LinkedList:
         # TODO: Otherwise raise error to tell user that delete has failed
         # Hint: raise ValueError('Item not found: {}'.format(item))
 
+        #check if list is empty and raise error if it is
+        if self.head is None: # O(1) time to check if node is empty
+            raise ValueError('List Empty.') # O(1) time to raise error
+
+        #check if item is the first item - delete execution differs for first item
+            #here we will address one item list as well
+        node = self.head # O(1) time for variable assignment
+        if node.data == item: # O(1) time to check if node.data == item
+            if node.next is not None: # O(1) time to check 
+                self.head = node.next # O(1) time for variable assignment
+            else: 
+                self.head = None # O(1) time for variable assignment
+                self.tail = None # O(1) time for variable assignment
+            return # O(1) time to terminate process flow
+
+        #here we will crawl along rest of list
+        while node is not None: # m+1 iterations (m being the position of the item being deleted) because we have to crawl along until item is found then also find the next item
+            next_node = node.next # O(1) time for variable assignment
+            if next_node is not None: # O(1) time to check
+                if next_node.data == item: # O(1) time to check
+                    new_next_node = next_node.next # O(1) time for variable assignment
+                    if new_next_node is not None:  # O(1) time to check
+                        node.next = new_next_node # O(1) time for variable assignment
+                    else: 
+                        self.tail = node # O(1) time for variable assignment
+                        node.next = None # O(1) time for variable assignment
+                    return # O(1) time to terminate process flow
+            node = node.next # O(1) time for variable assignment
+
+        raise ValueError('Item not found: {}'.format(item)) # O(1) time to raise error
+
+    def replace(self, matcher, replacer):
+        node = self.head  # O(1) time to assign new variable
+        # Loop until matcher is found
+        while node is not None:  # m iterations (m being the position of the item being replaced) because we have to crawl along until item is found
+            if node.data == matcher: # O(1) time to check
+                node.data = replacer  # O(1) time (on average) to append to list
+            node = node.next  # O(1) time to reassign variable
+        # Now list contains items from all nodes
 
 def test_linked_list():
     ll = LinkedList()
